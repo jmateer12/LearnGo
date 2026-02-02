7318b43b7d3202d20531592a516b172f1c0c048f
jmateer12 <jmateer@liberty.edu>
2026-02-01 22:16:41 -0500

    Learned more about implementing interfaces in golang

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -12,13 +12,13 @@ import (
 // Interfaces define high level organization of methods without business logic
 type Shape interface {
 	Area() float64
-	Abc()
 }
 
 type Rectangle struct {
 	width, height float64
 }
 
+// Implements Area() method for Rectangle
 func (r Rectangle) Area() float64 {
 	return r.width * r.height
 }
@@ -27,10 +27,42 @@ type Circle struct {
 	radius float64
 }
 
+// Implements Area() method for Circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+func calculateArea(s Shape) float64 {
+	return s.Area()
+}
+
 func main() {
-	fmt.Print("Hello")
+	//  This is how to define variables that contain struct data
+	rect := Rectangle{width: 5, height: 4}
+	circle := Circle{radius: 2}
+
+	fmt.Println(
+		"Rectangle area:",
+		calculateArea(rect),
+	)
+
+	fmt.Println(
+		"Circle area:",
+		calculateArea(circle),
+	)
+
+	mysteryBox := interface{}(10)
+	describeValue(mysteryBox)
+
+	retrievedInt, ok := mysteryBox.(int)
+
+	if ok {
+		fmt.Println("Retrieved int:", retrievedInt)
+	} else {
+		fmt.Println("Value is not an integer")
+	}
+}
+
+func describeValue(t interface{}) {
+	fmt.Printf("Type: %T, Value: %v\n", t, t)
 }
